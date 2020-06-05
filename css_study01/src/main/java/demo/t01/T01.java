fd8d6df0c332a32d2f8cf687748256d18b0d6aa2
Peter-cmd <2849308036@qq.com>
2020-06-05 22:09:19 +0800

    上传css_study01文件,内容是css练习,如:设置全屏背景图片,利用元素选择器设计网页样式.

---
+++ css_study01/src/main/java/demo/t01/T01.java
@@ -0,0 +1,10 @@
+package demo.t01;
+
+import java.io.File;
+
+public class T01 {
+    public static void main(String[] args){
+        File file = new File("");
+        System.out.println(file.getAbsolutePath());
+    }
+}
